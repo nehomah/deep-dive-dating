ca15cf021c1d52ffad8bda9fa93e85e92b989b3a
Nehomah Mora <nehomahm@gmail.com>
2019-02-06 19:45:59 -0700

    added sql directory and file https://app.asana.com/0/1108530030234345/1108530030234349

---
+++ sql/date-dan.sql
@@ -0,0 +1,32 @@
+ALTER	DATABASE beer CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS userDetail;
+DROP TABLE IF EXISTS question;
+DROP TABLE IF EXISTS answer;
+DROP TABLE IF EXISTS match;
+DROP TABLE IF EXISTS report;
+
+CREATE TABLE user (
+
+);
+
+CREATE TABLE userDetail (
+
+);
+
+CREATE TABLE question (
+
+);
+
+CREATE TABLE answer (
+
+);
+
+CREATE TABLE match (
+
+);
+
+CREATE TABLE report (
+
+);
