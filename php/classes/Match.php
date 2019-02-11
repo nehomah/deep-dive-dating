66d6dac253ec4c3931eefd916c3c3f38ab8f42ff
Taylor Smith <taylorleesmith92@gmail.com>
2019-02-11 13:02:37 -0700

    initial commit with some some boiler plate

---
+++ php/classes/Match.php
@@ -0,0 +1,19 @@
+<?php
+namespace unsure;
+require_once("autoload.php");
+require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
+use Ramsey\Uuid\Uuid;
+
+
+/**
+ * Match
+ *
+ * This class will be used to store and monitor when a user likes or matches with another user
+ *
+ * @author Taylor Smith
+ * @version 1
+ */
+
+class Match implements \JsonSerializable {
+
+}
