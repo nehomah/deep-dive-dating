dac12f81b7974a92a6765560da49181ead9cfd96
Kathleen Mattos <kathleenmmattos@gmail.com>
2019-02-11 14:18:29 -0700

    added user.php https://app.asana.com/0/inbox/1108530113740601/1108530030234348/1108533272808351

---
+++ php/classes/User.php
@@ -0,0 +1,65 @@
+<?php
+namespace DeepDiveDatingApp\DeepDiveDating;
+require_once ("autoload.php");
+require_once (dirname(__DIR__, 2) . "/vendor/autoload.php");
+
+use Ramsey\Uuid\Uuid;
+/**
+ * The user class will contain the identifying info for the account.
+ */
+class user implements \JsonSerializable {
+	use ValidateUuid;
+	/**
+	 * Id for the user's account, this is the primary key
+	 * @var string | Uuid $userId
+	 */
+	private $userId;
+	/**
+	 *activation token for the user account
+	 * @var string $userActivationToken
+	 */
+	private $userActivationToken;
+	/**
+	 * the browser and system info used by an account, recorded to help with the blocking/reporting process.
+	 * @var string $userAgent
+	 */
+	private $userAgent;
+	/**
+	 * url for the user avatar
+	 * @var string $userAvatarUrl
+	 */
+	private $userAvatarUrl;
+	/**
+	 *info on the blocked status on the account
+	 * @var string $userBlocked
+	 */
+	private $userBlocked;
+	/**
+	 * email address associated with the user account; this is unique
+	 * @var string $userEmail
+	 */
+	private $userEmail;
+	/**
+	 * handle for the user account, this is unique and should also be indexed
+	 * @var string $userHandle
+	 */
+	private $userHandle;
+	/**
+	 * encrypted password for the user account
+	 * @var string $userHash
+	 */
+	private $userHash;
+	/**
+	 * recorded ip address for the user account
+	 * @var string userIpAddress
+	 */
+	private $userIpAddress;
+
+	/**
+	 * Constructor for this user profile
+	 *
+	 * @param string|Uuid $newUserId id of this user or null if a new user account
+	 * @param string $newUserActivationToken activation token to safe guard against malicious accounts
+	 * @param string $newUserAgent  the recorded info about the browser and system to assist with blocking/reporting
+	 */
+}
