d2af8e63bcb07902ce5823aacc50bedd1004b8f0
Taylor Smith <taylorleesmith92@gmail.com>
2019-02-13 08:42:24 -0700

    finished adding params; added constructor; added accessors

--- php/classes/Report.php
+++ php/classes/Report.php
@@ -12,39 +12,39 @@ use Ramsey\Uuid\Uuid;
  * this class is used to store information regarding malicious user activity
  *
  * @author Taylor Smith
- */
+ **/
 
 class Report implements \JsonSerializable {
 	use ValidateUuid;
 	/**
 	 * id for the user who submitted the report
 	 * @var \Uuid $reportUserId
-	 */
+	 **/
 	private $reportUserId;
 	/**
 	 * id for the user accused of misconduct
 	 * @var \Uuid $reportAbuserId
-	 */
+	 **/
 	private $reportAbuserId;
 	/**
 	 * User Agent information
 	 * @var \string $reportAgent
-	 */
+	 **/
 	private $reportAgent;
 	/**
 	 * details of the incident being reported
 	 * @var \string $reportContent
-	 */
+	 **/
 	private $reportContent;
 	/**
 	 * date and time that the report was made
 	 * @var \DateTime $reportDate
-	 */
+	 **/
 	private $reportDate;
 	/**
 	 * Ip address of the user making the report
 	 * @var \Binary $reportIp
-	 */
+	 **/
 	private $reportIp;
 
 	/**
@@ -54,9 +54,83 @@ class Report implements \JsonSerializable {
 	 * @param string|Uuid $newReportAbuserId user id for the account detailed in the report
 	 * @param string $newReportAgent agent information for the user who made the report
 	 * @param string $newReportContent value/contents of the report
-	 * @param \DateTime
-	 * @param
-	 */
+	 * @param \DateTime|string $newReportDate date and time report was sent
+	 * @param string|Binary $newReportIp Ip address of user who submits this report
+	 * @throws \InvalidArgumentException if data type is invalid
+	 * @throws \RangeException if data values exceed limits
+	 * @throws \TypeError if data types violate type hints
+	 * @throws \Exception if some other exception occurs
+	 **/
+	public function __construct($newReportUserId, $newReportAbuserId, $newReportAgent, $newReportContent, $newReportDate, $newReportIp) {
+		try {
+			$this->setReportUserId($newReportUserId);
+			$this->setReportAbuserId($newReportAbuserId);
+			$this->setReportAgent($newReportAgent);
+			$this->setReportContent($newReportContent);
+			$this->setReportDate($newReportDate);
+			$this->setReportIp($newReportIp;)
+		}
+
+		catch (\InvalidArgumentException | \TypeError | \RangeException | \Exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+	}
+
+	/**
+	 * Accessor Method for Report User Id
+	 *
+	 * @return Uuid value of User Id for the person who made the report
+	 **/
+	public function getReportUserId() : Uuid {
+		return($this->reportUserId);
+	}
+
+	/**
+	 * Accessor Method for Report Abuser Id
+	 *
+	 * @return Uuid value of User Id for the person who the report is about
+	 **/
+	public function getReportAbuserId() : Uuid {
+		return($this->reportAbuserId);
+	}
+
+	/**
+	 * Accessor Method for Report Agent
+	 *
+	 * @return string user agent details
+	 **/
+	public function getReportAgent() : string {
+		return($this->reportAgent);
+	}
+
+	/**
+	 * Accessor Method for Report Content
+	 *
+	 * @return string details of incident being reported
+	 **/
+	public function getReportContent() : string {
+		return($this->reportContent);
+	}
+
+	/**
+	 * Accessor Method for Report Date
+	 *
+	 * @return \DateTime value of report date and time
+	 **/
+	public function getReportDate() : \DateTime {
+		return($this->reportDate);
+	}
+
+	/**
+	 * Accessor Method for Report Ip
+	 *
+	 * @return string|Binary Ip address of the person making the report
+	 **/
+	public function getReportIp() : string {
+		return($this->reportIp);
+	}
+
 }
 
 /**
