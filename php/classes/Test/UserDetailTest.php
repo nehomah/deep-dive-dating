abb73bd3ccd65db36409bcebd479163972219de5
Nehomah Mora <nehomahm@gmail.com>
2019-02-14 16:50:17 -0700

    added first section of userDetail test https://app.asana.com/0/1108530030234345/1108530030234349

--- php/classes/Test/UserDetailTest.php
+++ php/classes/Test/UserDetailTest.php
@@ -26,47 +26,47 @@ class UserDetailTest extends DeepDiveDatingAppTest {
 	 * valid about me for user
 	 * @var string $VALID_USERDETAILABOUTME
 	 **/
-	private $VALID_USERDETAILABOUTME = "This section is about me and no one else.";
+	protected $VALID_USERDETAILABOUTME = "This section is about me and no one else.";
 	/**
 	 * Valid age of user
 	 * @var int $VALID_USERDETAILAGE
 	 **/
-	private $VALID_USERDETAILAGE = 25;
+	protected $VALID_USERDETAILAGE = 25;
 	/**
 	 * Valid career info
 	 * @var string $VALID_USERDETAILCAREER
 	 **/
-	private $VALID_USERDETAILCAREER = "I have a career I love.";
+	protected $VALID_USERDETAILCAREER = "I have a career I love.";
 	/**
 	 * valid display email
 	 * @var string $VALID_USERDETAILDISPLAYEMAIL
 	 **/
-	private $VALID_USERDETAILDISPLAYEMAIL = "single@ready2mingle.com";
+	protected $VALID_USERDETAILDISPLAYEMAIL = "single@ready2mingle.com";
 	/**
 	 * valid education
 	 * @var string $VALID_USEREDUCATION
 	 **/
-	private $VALID_USEREDUCATION = "I've learned stuff about stuff.";
+	protected $VALID_USEREDUCATION = "I've learned stuff about stuff.";
 	/**
 	 * valid gender
 	 * @var string $VALID_USERDETAILGENDER
 	 **/
-	private $VALID_USERDETAILGENDER = "Mixed";
+	protected $VALID_USERDETAILGENDER = "Mixed";
 	/**
 	 * Valid interests
 	 * @var string $VALID_USERDETAILINTERESTS
 	 **/
-	private $VALID_USERDETAILINTERESTS = "I am interested in stuff and things.";
+	protected $VALID_USERDETAILINTERESTS = "I am interested in stuff and things.";
 	/**
 	 * Valid race
 	 * @var string $VALID_USERDETAILRACE
 	 **/
-	private $VALID_USERDETAILRACE = "Many races, mixed.";
+	protected $VALID_USERDETAILRACE = "Many races, mixed.";
 	/**
 	 * Valid religion
 	 * @var string $VALID_USERDETAILRELIGION
 	 **/
-	private $VALID_USERDETAILRELIGION = "I only believe in science.";
+	protected $VALID_USERDETAILRELIGION = "I only believe in science.";
 
 /*****************
  * Run the default setup operation for each test
