c8600e21c2888b67ac9d769819ec98bd58277809
Natalie Woodard <nwoodard1@cnm.edu>
2019-02-14 12:52:25 -0700

    added getAnswerByAnswerQuestionId and getAnswerByAnswerQuestionIdAndUserId for Answer class https://app.asana.com/0/inbox/1108530113740615/1108530030234351/1108533272808359

--- php/classes/Answer.php
+++ php/classes/Answer.php
@@ -270,7 +270,86 @@ class Answer implements \JsonSerializable {
 		}
 		return ($answer);
 	}
+	/**
+	 * gets the Answer by answerQuestionId
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @param Uuid|string $answerUserId answer user id
+	 * @return Answer|null Answer found or null if not found
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError when a variable are not the correct data type
+	 * @throws \RangeException if answer user id is not positive
+	 **/
+	public static function getAnswerByAnswerQuestionId(\PDO $pdo, $answerQuestionId): ?Answer {
+		// sanitize the answerQuestionId before searching
+		try {
+			$answerUserId = self::validateUuid($answerQuestionId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			throw(new \PDOException($exception->getMessage(), 0, $exception));
+		}
 
+		// create query template
+		$query = "SELECT answerUserId, answerQuestionId, answerResult, answerScore FROM answer WHERE answerQuestionId = :answerQuestionId";
+		$statement = $pdo->prepare($query);
+
+		// bind the answer user id to the place holder in the template
+		$parameters = ["answerQuestionId" => $answerQuestionId->getBytes()];
+		$statement->execute($parameters);
+
+		// grab the Answer from mySQL
+		try {
+			$answer = null;
+			$statement->setFetchMode(\PDO::FETCH_ASSOC);
+			$row = $statement->fetch();
+			if($row !== false) {
+				$answer = new Answer($row["answerUSerId"], $row["answerQuestionId"], $row["answerResult"], $row["answerScore"]);
+			}
+		} catch(\Exception $exception) {
+			// if the row couldn't be converted, rethrow it
+			throw(new \PDOException($exception->getMessage(), 0, $exception));
+		}
+		return ($answer);
+	}
+	/**
+	 * gets the Answer by answerQuestionIdAndUserId
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @param Uuid|string $getAnswerByAnswerQuestionIdAndUserId answer
+	 * @return Answer|null Answer found or null if not found
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError when a variable are not the correct data type
+	 * @throws \RangeException if answer user id is not positive
+	 **/
+	public static function getAnswerByAnswerQuestionIdAndUserId(\PDO $pdo, $getAnswerByAnswerQuestionIdAndUserId): ?Answer {
+		// sanitize the getAnswerByAnswerQuestionIdAndUserId before searching
+		try {
+			$getAnswerByAnswerQuestionIdAndUserId = self::validateUuid($getAnswerByAnswerQuestionIdAndUserId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			throw(new \PDOException($exception->getMessage(), 0, $exception));
+		}
+
+		// create query template
+		$query = "SELECT answerUserId, answerQuestionId, answerResult, answerScore FROM answer WHERE getAnswerByAnswerQuestionIdAndUserId = :getAnswerByAnswerQuestionIdAndUserId";
+		$statement = $pdo->prepare($query);
+
+		// bind the getAnswerByAnswerQuestionIdAndUserId to the place holder in the template
+		$parameters = ["getAnswerByAnswerQuestionIdAndUserId" => $getAnswerByAnswerQuestionIdAndUserId->getBytes()];
+		$statement->execute($parameters);
+
+		// grab the Answer from mySQL
+		try {
+			$answer = null;
+			$statement->setFetchMode(\PDO::FETCH_ASSOC);
+			$row = $statement->fetch();
+			if($row !== false) {
+				$answer = new Answer($row["answerUSerId"], $row["answerQuestionId"], $row["answerResult"], $row["answerScore"]);
+			}
+		} catch(\Exception $exception) {
+			// if the row couldn't be converted, rethrow it
+			throw(new \PDOException($exception->getMessage(), 0, $exception));
+		}
+		return ($answer);
+	}
 	/**
 	 * formats the state variables for JSON serialization
 	 *
