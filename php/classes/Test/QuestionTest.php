7c41f713a2a1290218c311f3ca074cce5c22e749
Natalie Woodard <nwoodard1@cnm.edu>
2019-02-14 14:33:30 -0700

    Began writing Question class test file https://app.asana.com/0/inbox/1108530113740615/1108530030234350/1108533272808357

--- php/classes/Test/QuestionTest.php
+++ php/classes/Test/QuestionTest.php
@@ -4,16 +4,35 @@
 namespace DeepDiveDatingApp\DeepDiveDating\QuestionTest;
 require_once("autoload.php");
 require_once(dirname(__DIR__,2)."/lib/uuid.php");
-use Ramsey\Uuid\Uuid;
+use DeepDiveDatingApp\DeepDiveDating\Question\Question;
 /**
  * unit test for the Question class
- * PDO methods are located in the Answer class
+ * PDO methods are located in the Question class
  * @ see php/classes/Question.php
  * @author Natalie Woodard
  */
-class AnswerTest extends AnswerTestSetup {
+class QuestionTest extends DeepDiveDatingAppTest {
 	/**
-	*
-	 * @var string
-	 */
+	* Questions for users
+	 * @var Question question
+	 **/
+	protected $question = null;
+
+	/**
+	 * valid id to create the question object to own the test
+	 *@var string $VALID_QUESTIONID
+	 **/
+protected $VALID_QUESTION_ID;
+
+	/**
+	 * Content of questions
+	 * @var string $VALID_QUESTIONCONTENT
+	 **/
+	protected $VALID_QUESTION_CONTENT = "PHPUnit test passing";
+
+	/**
+	 * Value applied to questions based on Dan's preferences
+	 * @var int $VALID_QUESTIONVALUE
+	 **/
+	protected $VALID_QUESTION_VALUE;
 }
