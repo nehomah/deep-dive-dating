9d5c53388e42a43c7c7f82ef2c95b05a2038c507
Nehomah Mora <nehomahm@gmail.com>
2019-02-15 00:12:23 -0700

    added lines 101-197 of userDetailTest https://app.asana.com/0/1108530030234345/1108530030234349

--- php/classes/Test/UserDetailTest.php
+++ php/classes/Test/UserDetailTest.php
@@ -85,7 +85,7 @@ class UserDetailTest extends DeepDiveDatingAppTest {
 	$userDetail = new UserDetail($userDetailId, $userDetailUserId, $this->VALID_ABOUT_ME, $this->VALID_AGE, $this->VALID_CAREER, $this->VALID_DISPLAY_EMAIL, $this->VALID_EDUCATION, $this->VALID_GENDER, $this->VALID_INTERESTS, $this->VALID_RACE, $this->VALID_RELIGION);
 	$userDetail->insert($this->getPDO());
 	// grab the data from mySQL and enforce the fields match our expectations
-	$pdoUserDetail = UserDetail::getUserDetailByUserDetailId()Id($this->getPDO(), 	$userDetail->getUserDetailId());
+	$pdoUserDetail = UserDetail::getUserDetailByUserDetailId($this->getPDO(),$userDetail->getUserDetailId());
 	$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("userDetail"));
 	$this->assertEquals($pdoUserDetail->getUserDetailId(), $userDetailId);
 	$this->assertEquals($pdoUserDetail->getUserDetailUserId(), $userDetailUserId);
@@ -93,8 +93,105 @@ class UserDetailTest extends DeepDiveDatingAppTest {
 	$this->assertEquals($pdoUserDetail->getUserDetailAge(), $this->VALID_AGE);
 	$this->assertEquals($pdoUserDetail->getUserDetailCareer(), $this->VALID_CAREER);
 	$this->assertEquals($pdoUserDetail->getUserDetailDisplayEmail(), $this->VALID_DISPLAY_EMAIL);
-	$this->assertEquals($newUserDetailEducation);
-	$this->assertEquals($newUserDetailGender);
-	$this->assertEquals($newUserDetailInterests);
-	$this->assertEquals($newUserDetailRace);
-	$this->assertEquals($newUserDetailReligion);
+	$this->assertEquals($pdoUserDetail->getUserDetailEducation(), $this->VALID_EDUCATION);
+	$this->assertEquals($pdoUserDetail->getUserDetailGender(), $this->VALID_GENDER);
+	$this->assertEquals($pdoUserDetail->getUserDetailInterests(), $this->VALID_INTERESTS);
+	$this->assertEquals($pdoUserDetail->getUserDetailRace(), $this->VALID_RACE);
+	$this->assertEquals($pdoUserDetail->getUserDetailReligion(), $this->VALID_RELIGION);
+	}
+	/**
+	* Test inserting a UserDetail, editing it and then updating it
+	**/
+	public function testUpdateValidUserDetail() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("userDetail");
+		// create a new UserDetail and insert into mySQL
+		$userDetailId = generateUuidV4();
+		$userDetail = new UserDetail($userDetailId, $userDetailUserId, $this->VALID_ABOUT_ME, $this->VALID_AGE, $this->VALID_CAREER, $this->VALID_DISPLAY_EMAIL, $this->VALID_EDUCATION, $this->VALID_GENDER, $this->VALID_INTERESTS, $this->VALID_RACE, $this->VALID_RELIGION);
+		$userDetail->insert($this->getPDO());
+		// edit the UserDetail and update it in mySQL
+		$userDetail->setUserDetailAboutMe($this->VALID_ABOUT_ME);
+		$userDetail->setUserDetailAge($this->VALID_AGE);
+		$userDetail->setUserDetailCareer($this->VALID_CAREER);
+		$userDetail->setUserDetailDisplayEmail($this->VALID_DISPLAY_EMAIL);
+		$userDetail->setUserDetailEducation($this->VALID_EDUCATION);
+		$userDetail->setUserDetailGender($this->VALID_GENDER);
+		$userDetail->setUserDetailInterests($this->VALID_INTERESTS);
+		$userDetail->setUserDetailRace($this->VALID_RACE);
+		$userDetail->setUserDetailReligion($this->VALID_RELIGION);
+		$userDetail->update($this->getPDO());
+	// grab the data from mySQL and enforce the fields match our expectations
+	$pdoUserDetail = UserDetail::getUserDetailByUserDetailId($this->getPDO(),$userDetail->getUserDetailId());
+	$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("userDetail"));
+	$this->assertEquals($pdoUserDetail->getUserDetailId(), $userDetailId);
+	$this->assertEquals($pdoUserDetail->getUserDetailUserId(), $userDetailUserId);
+	$this->assertEquals($pdoUserDetail->getUserDetailAboutMe(), $this->VALID_ABOUT_ME);
+	$this->assertEquals($pdoUserDetail->getUserDetailAge(), $this->VALID_AGE);
+	$this->assertEquals($pdoUserDetail->getUserDetailCareer(), $this->VALID_CAREER);
+	$this->assertEquals($pdoUserDetail->getUserDetailDisplayEmail(), $this->VALID_DISPLAY_EMAIL);
+	$this->assertEquals($pdoUserDetail->getUserDetailEducation(), $this->VALID_EDUCATION);
+	$this->assertEquals($pdoUserDetail->getUserDetailGender(), $this->VALID_GENDER);
+	$this->assertEquals($pdoUserDetail->getUserDetailInterests(), $this->VALID_INTERESTS);
+	$this->assertEquals($pdoUserDetail->getUserDetailRace(), $this->VALID_RACE);
+	$this->assertEquals($pdoUserDetail->getUserDetailReligion(), $this->VALID_RELIGION);
+}
+	/**
+ 	* Test creating a UserDetail and then deleting it
+	**/
+  public function testDeleteValidUserDetail() : void {
+  			// count the number of rows and save it for later
+			$numRows = $this->getConnection()->getRowCount("userDetail");
+
+			// create a new UserDetail and insert into mySQL
+			$userDetailId = generateUuidV4();
+			$userDetail = new UserDetail($userDetailId, $userDetailUserId, $this->VALID_ABOUT_ME, $this->VALID_AGE, $this->VALID_CAREER, $this->VALID_DISPLAY_EMAIL, $this->VALID_EDUCATION, $this->VALID_GENDER, $this->VALID_INTERESTS, $this->VALID_RACE, $this->VALID_RELIGION);
+			$userDetail->insert($this->getPDO());
+
+			// Delete the UserDetail from the mySQL
+			$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("userDetail"));
+			$userDetail->delete($this->getPDO());
+
+			// Grab the data from mySQL and enforce the UserDetail does not exist
+			$pdoUserDetail = UserDetail::getUserDetailByUserDetailId($this->getPDO(),$userDetail->getUserDetailId());
+			$this->assertNull(pdoUserDetail);
+			$this->assertEquals($numRows, $this->getConnection()->getRowCount("userDetail"));
+}
+
+/**
+ * Test inserting a UserDetail and regrabbing it from mySQL
+**/
+public function testGetValidUserDetailByUserDetailId() : void {
+			// count the number of rows and save it for later
+			$numRows = $this->getConnection()->getRowCount("userDetail");
+			// create a new UserDetail and insert into mySQL
+			$userDetailId = generateUuidV4();
+			$userDetail = new UserDetail($userDetailId, $userDetailUserId, $this->VALID_ABOUT_ME, $this->VALID_AGE, $this->VALID_CAREER, $this->VALID_DISPLAY_EMAIL, $this->VALID_EDUCATION, $this->VALID_GENDER, $this->VALID_INTERESTS, $this->VALID_RACE, $this->VALID_RELIGION);
+			$userDetail->insert($this->getPDO());
+			// grab the data from mySQL and enforce the fields match our expectations
+			// grab the data from mySQL and enforce the fields match our expectations
+			$pdoUserDetail = UserDetail::getUserDetailByUserDetailId($this->getPDO(),$userDetail->getUserDetailId());
+			$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("userDetail"));
+			$this->assertEquals($pdoUserDetail->getUserDetailId(), $userDetailId);
+			$this->assertEquals($pdoUserDetail->getUserDetailUserId(), $userDetailUserId);
+			$this->assertEquals($pdoUserDetail->getUserDetailAboutMe(), $this->VALID_ABOUT_ME);
+			$this->assertEquals($pdoUserDetail->getUserDetailAge(), $this->VALID_AGE);
+			$this->assertEquals($pdoUserDetail->getUserDetailCareer(), $this->VALID_CAREER);
+			$this->assertEquals($pdoUserDetail->getUserDetailDisplayEmail(), $this->VALID_DISPLAY_EMAIL);
+			$this->assertEquals($pdoUserDetail->getUserDetailEducation(), $this->VALID_EDUCATION);
+			$this->assertEquals($pdoUserDetail->getUserDetailGender(), $this->VALID_GENDER);
+			$this->assertEquals($pdoUserDetail->getUserDetailInterests(), $this->VALID_INTERESTS);
+			$this->assertEquals($pdoUserDetail->getUserDetailRace(), $this->VALID_RACE);
+			$this->assertEquals($pdoUserDetail->getUserDetailReligion(), $this->VALID_RELIGION);
+}
+
+/**
+ * Test grabbing a UserDetail that does not exist
+**/
+public function testGetInvalidUserDetailbyUserDetailId() : void {
+			// grab a userDetailId that exceeds the maximum allowable userDetailId
+			$fakeUserDetailId = generateUuidV4();
+			$userDetail = UserDetail::getUserDetailByUserDetailId($this->getPDO(), $fakeUserDetailId);
+			$this->assertNull($userDetail);
+}
+
+/** Test grabbing a UserDetail by userId */
